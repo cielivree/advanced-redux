96df326ac42d20d496aa25f2dd58cc51fca06e76
cielivree <cielivree93@gmail.com>
2025-10-06 19:17:04 +0300

    Custom hooks

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -2,5 +2,5 @@ import { TypedUseSelectorHook, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "../store/store";
 import { useSelector } from "react-redux";
 
-export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppDispatch = () => useDispatch<AppDispatch>()  // typed useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector  // App selector has a type of a root reducer
