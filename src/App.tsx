30a3191ca22982429d5cecbfb70fdad69da8cd22
cielivree <cielivree93@gmail.com>
2025-10-09 18:30:11 +0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import './App.css';
-import { userSlice } from './store/reducers/UserSlice';
+import React, { useEffect } from 'react';
+// import './App.css';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { userSlice } from './store/reducers/UserSlice';
+import { fetchUsers } from './store/reducers/ActionCreators';
 
 function App() {
   const dispatch = useAppDispatch()
-  const { count } = useAppSelector(state => state.userReducer)
+  const { count, users, isLoading, error } = useAppSelector(state => state.userReducer)
   const { increment } = userSlice.actions
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  }
+
+  if (error) {
+    return <h1>{error}</h1>
+  }
 
   return (
     <div className="App">
       <h1>{count}</h1>
       <button onClick={() => dispatch(increment(1))}>Click me</button>
+      <hr />
+      {users.map(user => (
+        <div>{user.name}</div>
+      ))}
     </div>
   );
 }
