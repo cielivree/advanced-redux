07e337c51fb7d8375de063ffc66b66fdda950b88
cielivree <cielivree93@gmail.com>
2025-10-12 19:35:48 +0300

    Post container

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ import { userSlice } from './store/reducers/UserSlice';
 import { fetchUsersAll } from './store/reducers/ActionCreators';
 
 function App() {
-  const dispatch = useAppDispatch()
-  const { count, users, isLoading, error } = useAppSelector(state => state.userReducer)
-  const { increment } = userSlice.actions
+const dispatch = useAppDispatch()
+const { count, users, isLoading, error } = useAppSelector(state => state.userReducer)
+const { increment } = userSlice.actions
 
-  useEffect(() => {
-    dispatch(fetchUsersAll())
-  }, []) 
-
+//  useEffect(() => {
+//    dispatch(fetchUsersAll())
+//  }, []) 
+ 
   return (
     <div className="App">
       <h1>{count}</h1>
       <button onClick={() =>  dispatch(increment(1))}>Click me</button>
       <hr />
-      {isLoading && <h1>Loading...</h1>}
-      {error && <h1>{error}</h1>}
-      {JSON.stringify(users, null, 2)}
+      {/*isLoading && <h1>Loading...</h1>*/}
+      {/*error && <h1>{error}</h1>*/}
+      {/*JSON.stringify(users, null, 2)*/}
     </div>
   );
 }
--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 
-export const userAPI = createApi({
-    reducerPath: 'userAPI',
+export const postAPI = createApi({
+    reducerPath: 'postAPI',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
     endpoints: (build) => ({
-        fetchAllUsers: build.query({
+        fetchAllPosts: build.query({
             query: () => ({
-                url: `/users`
+                url: `/posts`
             })
         })
     })
