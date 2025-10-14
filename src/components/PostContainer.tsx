de8828f6000507bbd41bbca164bcc76616801c5a
cielivree <cielivree93@gmail.com>
2025-10-14 17:48:12 +0300

    Building Post container

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { postAPI } from '../services/PostService'
 
 const PostContainer = () => {
+  const {data: posts} = postAPI.useFetchAllPostsQuery(5)
+  
   return (
-    <div>PostContainer</div>
+    <div>
+      <div className="post_list">
+        {posts?.map(post => 
+          <PostItem/>
+          )}
+      </div>
+    </div>
   )
 }
 
