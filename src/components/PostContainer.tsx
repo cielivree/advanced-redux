3604875564c8a548257df2202d62944e26f80f1d
cielivree <cielivree93@gmail.com>
2025-10-17 18:38:06 +0300

    RTK query features

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,16 +1,25 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { postAPI } from '../services/PostService'
 import PostItem from './PostItem'
 
 const PostContainer = () => {
   const [limit, setLimit] = useState(10)
-  const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(limit)
+  const { data: posts, error, isLoading, refetch } = postAPI.useFetchAllPostsQuery(limit, {
+    pollingInterval: 1000      // getting renewed data in interval
+  })
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLimit(5)
+    }, 5000)
+  }, [])
 
   return (
     <div>
       <div className="post__list">
         {isLoading && <h1>Loading...</h1>}
         {error && <h1>Something went wrong.</h1>}
+        <button onClick={() => refetch()}>Refetch</button>
         {posts?.map(post => 
           <PostItem post={post} />
           )}
