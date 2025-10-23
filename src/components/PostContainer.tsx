ad0884e344cda05e91b42d8325b0912289841b78
cielivree <cielivree93@gmail.com>
2025-10-23 18:12:22 +0300

    Finishing posts features

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,10 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import { postAPI } from '../services/PostService'
 import PostItem from './PostItem'
+import { IPost } from '../models/IPost'
 
 const PostContainer = () => {
   const [limit, setLimit] = useState(100)
   const { data: posts, error, isLoading, refetch } = postAPI.useFetchAllPostsQuery(limit)
+  const [updatePost, {}] = postAPI.useUpdatePostMutation()
+  const [deletePost, {}] = postAPI.useDeletePostMutation()
+
 //  {
 //    pollingInterval: 10000      // getting renewed data in interval
 //  })
@@ -15,7 +19,13 @@ const PostContainer = () => {
 //    }, 5000)
 // }, [])
 
-  
+  const handleRemove = (post: IPost) => {
+    deletePost(post)
+  }
+
+  const handleUpdate = (post: IPost) => {
+    updatePost(post)
+  }
 
   return (
     <div>
@@ -24,7 +34,7 @@ const PostContainer = () => {
         {error && <h1>Something went wrong.</h1>}
         <button onClick={() => refetch()}>Refetch</button>
         {posts?.map(post => 
-          <PostItem post={post} />
+          <PostItem post={post} remove={handleRemove} update={handleUpdate}  />
           )}
       </div>
     </div>
--- src/components/PostContainer2.tsx
+++ src/components/PostContainer2.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { postAPI } from '../services/PostService'
 import PostItem from './PostItem'
+import { IPost } from '../models/IPost'
 
 const PostContainer2 = () => {
-  const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(10)
+  const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(10)
+  const [updatePost, {}] = postAPI.useUpdatePostMutation()
+  const [deletePost, {}] = postAPI.useDeletePostMutation()
+
+  const handleRemove = (post: IPost) => {
+    deletePost(post)
+  }
+
+  const handleUpdate = (post: IPost) => {
+    updatePost(post)
+  }
+
   
   return (
     <div>
@@ -11,7 +23,7 @@ const PostContainer2 = () => {
         {isLoading && <h1>Loading...</h1>}
         {error && <h1>Something went wrong.</h1>}
         {posts?.map(post => 
-          <PostItem post={post} />
+          <PostItem post={post} remove={handleRemove} update={handleUpdate} />
           )}
       </div>
     </div>
